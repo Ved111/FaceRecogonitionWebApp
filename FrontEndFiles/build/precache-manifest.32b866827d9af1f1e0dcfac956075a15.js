ef91ce08ad6df53d0950baaeb3cd0969b6ca1fef
Ved111 <35655521+Ved111@users.noreply.github.com>
2020-01-12 14:30:28 +0530

    Add files via upload

---
+++ FrontEndFiles/build/precache-manifest.32b866827d9af1f1e0dcfac956075a15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf9af9600f72a8d954d42dc9449fac5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5271a4f98834c3f7791b",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4740ba3eb46152793c64",
+    "url": "/static/js/2.60a6da58.chunk.js"
+  },
+  {
+    "revision": "5271a4f98834c3f7791b",
+    "url": "/static/js/main.31a060aa.chunk.js"
+  },
+  {
+    "revision": "1450b706ab2197a19f56",
+    "url": "/static/js/runtime-main.a59ab6fb.js"
+  }
+]);
